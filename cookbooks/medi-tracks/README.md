4e9342456fa212214b59f06d6972a936d1a599ac
Valter Silva <valterhenrique85@gmail.com>
2017-03-11 10:35:09 +0100

    Create cookbook for media-tracks.

---
+++ cookbooks/medi-tracks/README.md
@@ -0,0 +1,4 @@
+# medi-tracks
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/medi-tracks/metadata.rb
@@ -0,0 +1,19 @@
+name 'medi-tracks'
+maintainer 'Valter Henrique'
+maintainer_email 'valterhenrique85@gmail.com'
+license 'all_rights'
+description 'Installs/Configures medi-tracks'
+long_description 'Installs/Configures medi-tracks'
+version '0.1.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+issues_url 'https://github.com/valterhenrique/medi-chef-repo/issues' if respond_to?(:issues_url)
+
+# The `source_url` points to the development reposiory for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+source_url 'https://github.com/valterhenrique/medi-chef-repo' if respond_to?(:source_url)
